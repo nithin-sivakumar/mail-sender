d5c98b3164857eea8ec3856027c4009d78ec2289
Nithin <nithinsgayathri@gmail.com>
2023-12-24 15:00:05 +0530

    Config Bug Fixes

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const { transporter } = require("./utils/mailConnect");
+const nodemailer = require("nodemailer");
+const { smtpHost, smtpPort } = require("../env");
 
 let USER, PASS;
+
+const config = (gmailId, googleAppPassword) => {
+  USER = gmailId;
+  PASS = googleAppPassword;
+};
+
+const transporter = nodemailer.createTransport({
+  host: smtpHost,
+  port: smtpPort,
+  secure: true, // use TLS
+  auth: {
+    user: USER,
+    pass: PASS,
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
+  },
+});
+
+console.log(
+  `Attempting to connect to SMTP server: ${smtpHost} at PORT: ${smtpPort}`
+);
 
 // Verify the connection
 transporter.verify(function (error, success) {
@@ -11,29 +36,30 @@ transporter.verify(function (error, success) {
   }
 });
 
-const config = (gmailId, googleAppPassword) => {
-  USER = gmailId;
-  PASS = googleAppPassword;
-};
-
 // SendMail endPoint
 const sendMail = (
   subject = "Sent using NodeMailer",
   content = "Test Email",
   sendTo
 ) => {
-  transporter.sendMail(
-    {
-      from: USER,
-      to: sendTo,
-      subject: subject,
-      html: content,
-    },
-    (err, info) => {
-      console.log(info.envelope);
-      console.log(info.messageId);
-    }
-  );
+  if (!USER || !PASS) {
+    console.log(
+      `Kindly use .config() before trying to send emails using sendMail()`
+    );
+  } else {
+    transporter.sendMail(
+      {
+        from: USER,
+        to: sendTo,
+        subject: subject,
+        html: content,
+      },
+      (err, info) => {
+        console.log(info.envelope);
+        console.log(info.messageId);
+      }
+    );
+  }
 };
 
 // Sample usage:
@@ -53,6 +79,4 @@ const sendMail = (
 module.exports = {
   sendMail,
   config,
-  USER,
-  PASS,
 };
--- utils/mailConnect.js
+++ utils/mailConnect.js
@@ -1,25 +1,32 @@
-const nodemailer = require("nodemailer");
-const { smtpHost, smtpPort } = require("../env");
-const { USER, PASS } = require("../index");
+// const nodemailer = require("nodemailer");
+// const { smtpHost, smtpPort } = require("../env");
 
-const transporter = nodemailer.createTransport({
-  host: smtpHost,
-  port: smtpPort,
-  secure: true, // use TLS
-  auth: {
-    user: USER,
-    pass: PASS,
-  },
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false,
-  },
-});
+// let USER, PASS;
 
-console.log(
-  `Attempting to connect to SMTP server: ${smtpHost} at PORT: ${smtpPort}`
-);
+// const setup = (gmailId, googleAppPassword) => {
+//   USER = gmailId;
+//   PASS = googleAppPassword;
+// };
 
-module.exports = {
-  transporter,
-};
+// const transporter = nodemailer.createTransport({
+//   host: smtpHost,
+//   port: smtpPort,
+//   secure: true, // use TLS
+//   auth: {
+//     user: USER,
+//     pass: PASS,
+//   },
+//   tls: {
+//     // do not fail on invalid certs
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// console.log(
+//   `Attempting to connect to SMTP server: ${smtpHost} at PORT: ${smtpPort}`
+// );
+
+// module.exports = {
+//   transporter,
+//   setup,
+// };
